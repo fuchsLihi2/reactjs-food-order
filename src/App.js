e2304aa7a4f59e8e1a329007bbe0368e37b05aee
Lihi Fuchs <lihifuchs@PAP-LihiFu-MacOS.local>
2022-04-20 08:22:02 +0300

    added cart visibility state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import {useState} from 'react';
+import { unstable_createPortal } from 'react-dom';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
+
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
